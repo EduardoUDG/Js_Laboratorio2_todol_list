4c58d13c919276bc2c6b19c2e7a6b2d1401b14e3
EduardoUbuntu <eduardo.cdorado@alumnos.udg.mx>
2021-10-12 11:11:46 -0500

    TodoList class

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 
-import { saludar } from './js/componentes.js'
-import { Todo } from './classes/todo.class.js'
-
 import './style.css';
+import { saludar } from './js/componentes.js'
+
+import { Todo, TodoList } from './classes'
+
+const todoList = new TodoList();
 
 const tarea = new Todo('Aprender Javascript');
 console.log( tarea );
+
+todoList.nuevoTodo( tarea );
+console.log( todoList );
